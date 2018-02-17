1694a1fdabf4746eaa021460308088cda759f75e
misterwolf <dario.gas@gmail.com>
2018-02-17 19:01:21 +0100

    first setup up of this amazing project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserBaseOps::Application.config.secret_key_base = 'c9719f731d0aa071423e4aaf600c73aacbfe92c5b73c9a69811a5ee0dc8ea1e6e89aec624ddb5220ebb28f04c27a1ae599bc3eb6c3cb7ae2d7efdc36ccd62a15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UserBaseOps::Application.config.session_store :cookie_store, key: '_user_base_ops_session'
